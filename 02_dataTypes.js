3978b880588db3deb6a00cc517a5cc3cd35bcf5d
Soumitra Samanta <soumitrasamanta506@gmail.com>
2024-03-05 23:23:14 +0530

    data types done...

---
+++ 02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello")  // we are using nodejs, not browser
+
+let name = "soumitra"
+let age = 19
+let isClickable = false
+
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
