9f6e41730008887f4e1fd09ab46a4e272136294d
Soumitra Samanta <soumitrasamanta506@gmail.com>
2024-03-07 23:25:16 +0530

    Number and Math done...

---
+++ 07_nums_math.js
@@ -0,0 +1,28 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100) // explicitly define to be number
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNum = 123.8966
+// console.log(otherNum.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************************************ Maths ****************************************************************
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(1.2));
+
+console.log(Math.random()); // random value b/w (0, 1)
+console.log(Math.random()*10);
+
+const min = 10
+const max = 20
+console.log((Math.random()*(max-min+1)) + min);
